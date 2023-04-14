09d25574949a73bfd8bf7e8a9b048bf04b0b3028
Mark.Connelly <mark.d.connelly@outlook.com>
2023-04-14 11:18:57 -0500

    Update Out-MDCToCSV.ps1

--- Functions/Utility/Out-MDCToCSV.ps1
+++ Functions/Utility/Out-MDCToCSV.ps1
@@ -5,7 +5,7 @@
     This module frequently creates psobjects that are used for reporting. This function will take a psobject and output it to the file path in the parameters with a unique file name.
 .NOTES
     This is a custom function written by Mark Connelly, so it may not work as intended.
-    This function assumes a valid file path and name attribtue are passed to it. If it is not, the function will fail.
+    This function assumes a valid psobj, file path, and name attribtue are passed to it. If it is not, the function will fail.
 .LINK
     N/A
 .EXAMPLE
