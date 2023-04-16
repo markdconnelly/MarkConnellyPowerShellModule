935b49ee1ca72848ba409d57fda27dd95eaac7a1
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-16 17:22:32 -0500

    Update Set-MDCSecAttrHashTable.ps1

--- Functions/AzureAD/CustomSecurityAttributes/Set-MDCSecAttrHashTable.ps1
+++ Functions/AzureAD/CustomSecurityAttributes/Set-MDCSecAttrHashTable.ps1
@@ -5,13 +5,12 @@
     This function will create a hash table with the custom security attribute definition that is passed to it.
 .NOTES
     This is a custom function written by Mark Connelly, so it may not work as intended. Use at your own risk.
-    This function assumes a secret store with the appropriate variables is in place. If it is not, the function will fail.
 .LINK
     https://github.com/markdconnelly/MarkConnellyPowerShellModule/blob/main/Functions/AzureAD/CustomSecurityAttributes/Set-MDCSecAttrHashTable.ps1
 .EXAMPLE
     Set-MDCSecAttrHashTable -AttributeSet "Set1" -AttributeName "Attribute1" -AttributeValue "Value1"
 #>
-Function Set-UserCustomSecurityAttribute{
+Function Set-MDCSecAttrHashTable{
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory=$true,Position=0)]
