19fd9c339fa2b9cf3cd9105d92d23c91897360ab
Mark.Connelly <mark.d.connelly@outlook.com>
2023-04-17 16:12:39 -0500

    Update FunctionTemplate.ps1

---
+++ Functions/FunctionTemplate.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    <Quick overview of the function>
+.DESCRIPTION
+    <More detailed description of the function>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+.NOTES
+    This is a custom function written by Mark Connelly, so it may not work as intended.
+    Version:        1.0
+    Author:         <Name>
+    Last Updated:   <Date>
+    Creation Date:  <Date>
+    Purpose/Change: Initial script development
+.LINK
+    https://github.com/markdconnelly/MarkConnellyPowerShellModule/blob/main/Functions/Utility/Connect-MDCGraphApplication.ps1
+.EXAMPLE
+    Connect-MDCGraphApplication 
+    Connect-MDCGraphApplication -ProductionEnvironment $false
+    Connect-MDCGraphApplication -ProductionEnvironment $true
+#>
