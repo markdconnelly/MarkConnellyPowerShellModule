79ed86acddf6539a463bb24e297105fadca0fd08
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-16 22:41:14 -0500

    Update Get-FlaggedGraphPermissionArray.ps1

--- Functions/Utility/Get-FlaggedGraphPermissionArray.ps1
+++ Functions/Utility/Get-FlaggedGraphPermissionArray.ps1
@@ -15,6 +15,39 @@ Function Get-FlaggedAzureADRoleArray {
     [CmdletBinding()]
     Param ()
 
-    $arrFlaggedAzureADRoles = Get-MgDirectoryRole | Where-Object {$_.DisplayName -like "*Administrator*"} | Select-Object Id, DisplayName, Description
-    return $arrFlaggedAzureADRoles
+    $GraphAppId = "00000003-0000-0000-c000-000000000000"
+    $GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '$GraphAppId'"
+    $GraphServicePrincipal | ConvertTo-Json -Depth 10 | Out-File -LiteralPath "C:\temp\GraphServicePrincipal.json" -Force
+    
+    $flaggedAppRoles = $GraphServicePrincipal.AppRoles | Where-Object {($_.Value -like "*AccessReview*" -or `
+                                                                        $_.Value -like "*AdministrativeUnit*" -or `
+                                                                        $_.Value -like "*APIConnectors*" -or `
+                                                                        $_.Value -like "*Application*" -or `
+                                                                        $_.Value -like "*AppRoleAssignment*" -or `
+                                                                        $_.Value -like "*CustomSecAttribute*" -or `
+                                                                        $_.Value -like "*AuditLog*" -or `
+                                                                        $_.Value -like "*AuthenticationContext*" -or `
+                                                                        $_.Value -like "*BitlockerKey*" -or `
+                                                                        $_.Value -like "*ChannelMessage*" -or `
+                                                                        $_.Value -like "*Contacts*" -or `
+                                                                        $_.Value -like "*ConsentRequest*" -or `
+                                                                        $_.Value -like "*Device*" -or `
+                                                                        $_.Value -like "*Directory.ReadWrite*" -or `
+                                                                        $_.Value -like "*Domain*" -or `
+                                                                        $_.Value -like "*Files.Read*" -or `
+                                                                        $_.Value -like "*Group*" -or `
+                                                                        $_.Value -like "*Identity*" -or `
+                                                                        $_.Value -like "*DeviceManagementApps*" -or `
+                                                                        $_.Value -like "*DeviceManagementConfiguration*" -or `
+                                                                        $_.Value -like "*DeviceManagementManagedDevices*" -or `
+                                                                        $_.Value -like "*DeviceManagementRBAC*" -or `
+                                                                        $_.Value -like "*Mail*" -or `
+                                                                        $_.Value -like "*Organization*" -or `
+                                                                        $_.Value -like "*PrivilegedAccess*" -or `
+                                                                        $_.Value -like "*Policy*" -or `
+                                                                        $_.Value -like "*RoleManagement*" -or `
+                                                                        $_.Value -like "*Security*" -or `
+                                                                        $_.Value -like "*User*") -and `
+                                                                        $_.AllowedMemberTypes -contains "Application"} `
+                                                                        | Select-Object Id, Value, DisplayName
 }
