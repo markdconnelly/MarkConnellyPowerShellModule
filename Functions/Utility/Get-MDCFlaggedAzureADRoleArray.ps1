2d2c7c30e64c99368571992ae8ccc0b8d366f0a2
Mark.Connelly <mark.d.connelly@outlook.com>
2023-04-17 08:24:06 -0500

    Update Get-MDCFlaggedAzureADRoleArray.ps1

--- Functions/Utility/Get-MDCFlaggedAzureADRoleArray.ps1
+++ Functions/Utility/Get-MDCFlaggedAzureADRoleArray.ps1
@@ -5,6 +5,10 @@
     Produces an array of flagged Azure AD roles to be in used in comparison operations.
 .NOTES
     This is a custom function written by Mark Connelly, so it may not work as intended.
+    Version:        1.0
+    Author:         Mark D. Connelly Jr.
+    Creation Date:  04-16-2023
+    Purpose/Change: Initial script development
 .LINK
     https://github.com/markdconnelly/MarkConnellyPowerShellModule/blob/main/Functions/Utility/Get-FlaggedAzureADRoleArray.ps1
 .EXAMPLE
@@ -23,7 +27,7 @@ Function Get-FlaggedAzureADRoleArray {
 
     # Export the array of applications to a csv file if an export path is provided
     if($ExportPath){
-        Out-MDCToCSV -psobj $arrFlaggedAzureADRoles -ExportPath $ExportPath -FileName "GraphAPI_FlaggedPermissionArray"
+        Out-MDCToCSV -psobj $arrFlaggedAzureADRoles -ExportPath $ExportPath -FileName "AAD_FlaggedRoleArray"
     }
     return $arrFlaggedAzureADRoles
 }
