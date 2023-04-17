bf29e9416d5ece855234acfa631fa9377f2240c8
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-16 19:09:41 -0500

    Update Set-MDCBulkGroupMembership.ps1

--- Functions/Active Directory/Set-MDCBulkGroupMembership.ps1
+++ Functions/Active Directory/Set-MDCBulkGroupMembership.ps1
@@ -1,6 +1,10 @@
 <#
 .SYNOPSIS
-    This function will add an array of users to the provided AD group.
+    This function will add an array of users to the provided AD group. The column header that is passed must be "UserID" and it must be formatted in one of the following formats.
+    1. SamAccountName
+    2. DN
+    3. GUID
+    4. SID
 .DESCRIPTION
     Bulk adding users to new groups is a common task. This function will add an array of users to the provided AD group.
 .NOTES
@@ -24,26 +28,26 @@ Function Set-MDCBulkGroupMembership{
         [string]$GroupName
     )
 
-    # Collect array of application service principals
-    $arrAAD_Users = @()
-    try {
-        Write-Verbose "Collecting AAD Users"
-        $arrAAD_Users = Get-MgUser -All:$true -ErrorAction Stop `
-        | Where-Object {$_.UserPrincipalName -notlike "svc*" `
-                -and $_.UserPrincipalName -notlike "*Mailbox*" `
-                -and $_.UserPrincipalName -notlike "Sync_*" `
-                -and $_.UserPrincipalName -notlike "*BreakGlass*"}
-    }
-    catch {
-        Write-Verbose "Unable to get AAD Users"
-        throw "Unable to get AAD Users"
-    }
+    $intProgress = 1
+    $arrResolvedUsers = @()
+    foreach($user in $UserArray){
+        ##Progress Bar
+        Write-Progress `
+            -Activity 'Processing' `
+            -Status "$intProgress of $($UserArray.Count)" `
+            -CurrentOperation $intProgress `
+            -PercentComplete (($intProgress / @($UserArray).Count) * 100)
 
-    # Export the array of applications to a csv file if an export path is provided
-    if($ExportPath){
-        Out-MDCToCSV -psobj $arrAAD_Users -ExportPath $ExportPath -FileName "AAD_Users"
+        ##Attempt to resolve user from the provided array
+        $arrUser = $null
+        $arrUser = Get-ADUser -Identity $user.UserID -ErrorAction SilentlyContinue
+        if($arrUser -ne $null){
+            $arrResolvedUsers += $arrUser
+        }
+        $intProgress ++
     }
-
-    # Return the array of applications
-    return $arrAAD_Users
+    if($arrResolvedUsers.Count -ne $UserArray.Count){
+        # Alert and exit function
+        Write-Host "Number of resolved users:""$($arrResolvedUsers.Count)"" does not match number of users in input:""$($UserArray.Count)""; Stopping..." `
+    }
 }
