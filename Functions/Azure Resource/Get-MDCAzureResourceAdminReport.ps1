fcc39fa65fef0311d521abae1e04fe7a2f23bf5e
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-18 18:23:45 -0500

    Update Get-MDCAzureResourceAdminReport.ps1

--- Functions/Azure Resource/Get-MDCAzureResourceAdminReport.ps1
+++ Functions/Azure Resource/Get-MDCAzureResourceAdminReport.ps1
@@ -1,8 +1,8 @@
 <#
 .SYNOPSIS
-    This function will produce an executive summary of a tenant's conditional access policies.
+    This function will produce a psobj with admins and their various permissions. f a tenant's conditional access policies. If an export path parameter is provided, the function will export the results to a csv file.
 .DESCRIPTION
-    Identifying which controls are enforced in various conditional access policies is a challenge. This executive summary provides a quick report on the high level details. 
+    Identifying excessive admin permissions is the goal. This executive summary provides a quick report on the high level details. 
 .NOTES
     This is a custom function written by Mark Connelly, so it may not work as intended.
     This function assumes a connection to the Graph API exists. If it does not, the function will fail.
