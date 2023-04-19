f7dc4140e6efbd2587fe3f34fb5967c69c066d45
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-18 19:18:54 -0500

    Update Get-MDCAzureResourceAdminReport.ps1

--- Functions/Azure Resource/Get-MDCAzureResourceAdminReport.ps1
+++ Functions/Azure Resource/Get-MDCAzureResourceAdminReport.ps1
@@ -2,10 +2,13 @@
 .SYNOPSIS
     This function will produce a psobj with admins and their various permissions. f a tenant's conditional access policies. If an export path parameter is provided, the function will export the results to a csv file.
 .DESCRIPTION
-    Identifying excessive admin permissions is the goal. This executive summary provides a quick report on the high level details. 
+    Identifying excessive admin permissions is the goal. This report will provide the information that you need to identify and remediate excessive permissions in the Azure Resource Manager.
 .NOTES
     This is a custom function written by Mark Connelly, so it may not work as intended.
-    This function assumes a connection to the Graph API exists. If it does not, the function will fail.
+    Version:        1.0
+    Author:         Mark D. Connelly Jr.
+    Creation Date:  04-18-2023
+    Purpose/Change: Initial script development
 .LINK
     https://github.com/markdconnelly/MarkConnellyPowerShellModule/blob/main/Functions/AzureAD/ConditionalAccess/Get-MDCConditionalAccessExecutiveSummary.ps1
 .EXAMPLE
@@ -22,6 +25,83 @@ Function Get-MDCAzureResourceAdminReport {
         [bool]$ProductionEnvironment = $false
     )
 
+    #region Connect to the Azure Resource Manager
+    try {
+        Write-Verbose "Connecting to the Azure Resource Manager"
+        Connect-MDCAzApplication -ProductionEnvironment $ProductionEnvironment -ErrorAction Stop
+    }
+    catch {
+        Write-Verbose "Unable to connect to the Azure Resource Manager"
+        throw "Unable to connect to the Azure Resource Manager"
+    }#endregion
+
+    #region Collect Azure Role Assignments at the Management Group Scope
+    $arrAzureManagementGroups = @()
+    try {
+        $arrAzureManagementGroups = Get-AzManagementGroup -ErrorAction Stop
+    }
+    catch {
+        Write-Verbose $Error[0].Exception.Message
+        Write-Verbose "Unable to retrieve Azure Management Groups"
+    }
+    foreach($managementGroup in $arrAzureManagementGroups){
+        $arrManagementGroupRoleAssignments = @()
+        $arrManagementGroupRoleAssignments = Get-AzRoleAssignment -Scope $managementGroup.GroupId | Where-Object {$_.Scope -eq $managementGroup.GroupId}
+        foreach($roleAssignment in $arrManagementGroupRoleAssignments){
+            if($roleAssignment.ObjectType -like "*group*"){ #If role assignment is a group, get the members of the group
+                Write-Verbose "$($roleAssignment.DisplayName) is a group"
+                $arrGroupMembers = @()
+                try {
+                    #try: Get-MgGroupMember
+                    Write-Verbose "Collecting members of group $($roleAssignment.DisplayName)"
+                    $arrGroupMembers = Get-MgGroupMember -GroupId $roleAssignment.ObjectId -ErrorAction Stop
+                    $groupMember = ""
+                    foreach($groupMember in $arrGroupMembers){
+                        # For each member, add a new object to the array
+                        $psobjRoles += [PSCustomObject]@{
+                            RoleType = "Azure"
+                            Scope = "Management Group"
+                            ResourceId = $managementGroup.GroupId
+                            ResourceName = $managementGroup.DisplayName
+                            ResourceType = "Management Group"
+                            RoleName = $roleAssignment.RoleDefinitionName
+                            MemberName = $groupMember.AdditionalProperties.userPrincipalName
+                            MemberType = "Group - $($roleAssignment.DisplayName)"
+                            MemberUpn = $groupMember.AdditionalProperties.userPrincipalName
+                            MemberObjId = $roleAssignment.ObjectId
+                        }
+                        $pause = ""
+                    }
+                }
+                catch {
+                    #catch: Get-MgGroupMember
+                    Write-Verbose "Unable to get members of group $($roleAssignment.DisplayName)"
+                }
+            }else{ #If role assignment is a user, proceed as normal
+                $psobjRoles += [PSCustomObject]@{
+                    RoleType = "Azure"
+                    Scope = "Management Group"
+                    ResourceId = $managementGroup.GroupId
+                    ResourceName = $managementGroup.DisplayName
+                    ResourceType = "Management Group"
+                    RoleName = $roleAssignment.RoleDefinitionName
+                    MemberName = $roleAssignment.DisplayName
+                    MemberType = "Direct"
+                    MemberUpn = $roleAssignment.SignInName
+                    MemberObjId = $roleAssignment.ObjectId
+                }
+            }
+        }
+    }#endregion
+
+
+
+
+
+
+
+
+
     # Put function here
     #
     #
@@ -31,3 +111,5 @@ Function Get-MDCAzureResourceAdminReport {
 
     return $psobjAzureResourceAdminReport
 }
+
+Get-MDCAzureResourceAdminReport 
