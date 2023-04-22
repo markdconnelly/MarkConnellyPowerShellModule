5da4a7483377aadf52d83480cdd755abed7b5d22
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-21 19:48:01 -0500

    Update MarkConnellyPowerShellModule.psm1
    Minor adjustments to module checking logic

--- MarkConnellyPowerShellModule.psm1
+++ MarkConnellyPowerShellModule.psm1
@@ -13,7 +13,7 @@ $requiredModules += @(
 Write-Verbose "Required modules: $requiredModules"
 
 # Export nothing to clear implicit exports
-Export-ModuleMember
+# Export-ModuleMember
 
 # Perform dependent module checks
 
@@ -26,19 +26,18 @@ foreach($module in $requiredModules){
 
         # If module is not imported, but available on disk then import
         if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module}) {
-            Import-Module $module -Verbose
+            Write-Verbose "Module $module is available for importing"
         }
         else {
 
             # If module is not imported, not available on disk, but is in online gallery then install and import
             if (Find-Module -Name $module | Where-Object {$_.Name -eq $module}) {
-                Install-Module -Name $module -Force -Verbose -Scope AllUsers
-                Import-Module $module -Verbose
+                Write-Error "Module $module not installed, but available in an online gallery. Please install the module and try again."
             }
             else {
 
                 # If the module is not imported, not available and not in the online gallery then abort
-                Write-Error "Module $module not imported, not available and not in an online gallery, exiting." -ErrorAction Stop
+                Write-Error "Module $module not imported, not available and not in an online gallery, exiting."
             }
         }
     }
@@ -67,4 +66,4 @@ Foreach ($function in $functionFiles){
 }
  
 # Export everything in the public folder
-Export-ModuleMember -Function * -Alias * -Cmdlet *
+# Export-ModuleMember -Function * -Alias * -Cmdlet *
