90184aa9acba06ceaedfef9ed97b982d43089709
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-21 21:06:24 -0500

    Update MarkConnellyPowerShellModule.psm1
    Remove module dependency checks. slows down module load drastically.

--- MarkConnellyPowerShellModule.psm1
+++ MarkConnellyPowerShellModule.psm1
@@ -1,49 +1,8 @@
 # Universal psm file
 # Requires -Version 5.1
 
-# Set Module Dependencies
-$requiredModules = @()
-$requiredModules += @(
-    'Az',
-    'Microsoft.Graph',
-    'Microsoft.PowerShell.SecretStore',
-    'Microsoft.PowerShell.SecretManagement',
-    'ActiveDirectory'
-)
-Write-Verbose "Required modules: $requiredModules"
-
 # Export nothing to clear implicit exports
 Export-ModuleMember
-
-# Perform dependent module checks
-
-# Loop through all required modules and check if they are imported
-foreach($module in $requiredModules){
-    if (Get-Module | Where-Object {$_.Name -eq $module}) {
-        Write-Verbose "Module $module is already imported."
-    }
-    else {
-
-        # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module}) {
-            Write-Verbose "Module $module is available for importing"
-        }
-        else {
-
-            # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $module | Where-Object {$_.Name -eq $module}) {
-                Write-Error "Module $module not installed, but available in an online gallery. Please install the module and try again."
-            }
-            else {
-
-                # If the module is not imported, not available and not in the online gallery then abort
-                Write-Error "Module $module not imported, not available and not in an online gallery, exiting."
-            }
-        }
-    }
-
-}
-
 
 # Get function files
 $functionPath = ""
