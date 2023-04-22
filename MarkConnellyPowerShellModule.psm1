5e9bb4d17647c346b35742a2d31dbab5770dab31
Mark Connelly <mark.d.connelly@outlook.com>
2023-04-21 19:33:34 -0500

    Update MarkConnellyPowerShellModule.psm1
    Added error handling to get required modules and bail out if they cannot be loaded.

--- MarkConnellyPowerShellModule.psm1
+++ MarkConnellyPowerShellModule.psm1
@@ -1,8 +1,50 @@
 # Universal psm file
 # Requires -Version 5.1
 
+# Set Module Dependencies
+$requiredModules = @()
+$requiredModules += @(
+    'Az',
+    'Microsoft.Graph',
+    'Microsoft.PowerShell.SecretStore',
+    'Microsoft.PowerShell.SecretManagement',
+    'ActiveDirectory'
+)
+Write-Verbose "Required modules: $requiredModules"
+
 # Export nothing to clear implicit exports
 Export-ModuleMember
+
+# Perform dependent module checks
+
+# Loop through all required modules and check if they are imported
+foreach($module in $requiredModules){
+    if (Get-Module | Where-Object {$_.Name -eq $module}) {
+        Write-Verbose "Module $module is already imported."
+    }
+    else {
+
+        # If module is not imported, but available on disk then import
+        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module}) {
+            Import-Module $module -Verbose
+        }
+        else {
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            if (Find-Module -Name $module | Where-Object {$_.Name -eq $module}) {
+                Install-Module -Name $module -Force -Verbose -Scope AllUsers
+                Import-Module $module -Verbose
+            }
+            else {
+
+                # If the module is not imported, not available and not in the online gallery then abort
+                Write-Error "Module $module not imported, not available and not in an online gallery, exiting." -ErrorAction Stop
+            }
+        }
+    }
+
+}
+
 
 # Get function files
 $functionPath = ""
